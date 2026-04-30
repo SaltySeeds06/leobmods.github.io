0c5f1128cd7ce0b824754170cece8f03aaac3fe0
SaltySeeds06 <saltyseedz@gmail.com>
2026-04-30 16:57:41 +0100

    Website update

---
+++ mods.js
@@ -0,0 +1,220 @@
+const MODS = [
+  {
+    id: "ets2-global-background-map",
+    title: "ETS2 Global Background Map",
+    category: "UI",
+    author: "SaltySeeds06",
+    images: ["images/ets2-global-background-map/00.png", "images/ets2-global-background-map/01.png", "images/ets2-global-background-map/02.png", "images/ets2-global-background-map/03.png", "images/ets2-global-background-map/04.png"],
+    description: `<p><strong>General Information:</strong></p><p>This background map has full coverage of nearly the entire world</p><p>It adds all of Africa Europe and Asia along with Australia + New Zealand, North America and South America.</p><p>If you have any suggestions or notice any errors, write a comment!</p><p><br></p><p><strong>Notes for Map Modders:</strong></p><p>The majority of the map is based on the Lambert Conic Conformal projection (North America, Asia and Africa above the equator, excluding the mediterranean coast, all follow it exactly) with major adjustments to Europe. Antarctica is entirely fictional</p><p>Australia is based on the same projection however it has been shrunk and moved left due to the Island of Tasmania being beyond the limit that the game's coordinate system can support.</p><p>South America is based on a custom projection since LCC would make it too large to practically use.</p><p><span style="color: rgb(240, 102, 102);"></p><p><br></p><p><strong>Credits</strong></p><p>Software used: paint.net and QGIS</p><p>During the background map's development it was based on the AllWorld background by Freeman with the borders and outlines of countries have been completely redone from scratch. Full credit to him, and GMC Community for maintaing his background, along with the author of the World's Combined background, Melphiz, and to DOWL for creating the Promods background</p><p>Full credit to BenganJ (rest in peace) who's knowlege of background generation/creation was used extensively for this mod</p>`,
+    etsCompat: "All Game Versions",
+    downloads: [
+      {
+        label: "Download v2.7",
+        url: "https://modsfire.com/MnIGYS848oCsCIx/",
+        changelog: "<p><strong>v2.7</strong></p><ul><li>Made many fixes and changes, too many to list here and I have forgot most of them anyway</li><li>Big thanks to MERAS for the comprehensive list of suggestions and improvements to the background map!</li></ul>"
+      },
+    ],
+  },
+
+  {
+    id: "ets2-global-background-map-grand-utopia",
+    title: "ETS2 Global Background Map - Grand Utopia+",
+    category: "UI",
+    author: "SaltySeeds06",
+    images: ["images/ets2-global-background-map-grand-utopia/00.png", "images/ets2-global-background-map-grand-utopia/01.png","images/ets2-global-background-map/01.png", "images/ets2-global-background-map-grand-utopia/02.png", "images/ets2-global-background-map-grand-utopia/03.png", "images/ets2-global-background-map-grand-utopia/04.png"],
+    description: `<p><strong>General Information:</strong></p><p>This background map has 
+full coverage of nearly the entire world including fictional maps such 
+as Grand Utopia, Dreamland Map and fictionally positioned FSG and PJ 
+Indo</p><p>It also adds the same territories as my other backgrounds 
+adding all of Africa Europe and Asia along with Australia + New Zealand 
+and North America with a small part of northern South America, 
+(including Colombia Venezuela, Guyana, Suriname, French Guiana, Peru, 
+Ecuador, Bolivia, Paraguay, nearly all of Brazil and parts of Argentina 
+and Chile)</p><p>If you have any suggestions or notice any errors, write a comment!</p><p><br></p><p><strong>Notes for Map Modders:</strong></p><p>The
+ majority of the map is based on the Lambert Conic Conformal projection 
+(North America, Asia and Africa above the equator, excluding the 
+mediterranean coast, all follow it exactly) with major adjustments to 
+Europe. Antarctica is entirely fictional</p><p>Australia is based on the
+ same projection however it has been shrunk and moved left due to the 
+Island of Tasmania being beyond the 32 bit limit that the game's 
+coordinate system can support.</p><p>South America is based on a custom projection since LCC would make it too large to practically use.</p><p><span style="color: rgb(240, 102, 102);"></p><p><br></p><p><strong>Credits</strong></p><p>Software used: paint.net and QGIS</p><p>During
+ the background map's development it was based on the AllWorld 
+background by Freeman with the borders and outlines of countries have 
+been completely redone from scratch. Full credit to him, and GMC 
+Community for maintaing his background, along with the author of the 
+World's Combined background, Melphiz, and to DOWL for creating the 
+Promods background</p><p>Full credit to BenganJ (rest in peace) who's knowlege of background generation/creation was used extensively for this mod</p>`,
+    etsCompat: "All Game Versions",
+    downloads: [
+      {
+        label: "Download v2.7",
+        url: "https://modsfire.com/L44OywT3nxOnBN0/",
+        changelog: "<p><strong>v2.7</strong></p><ul><li>Made many fixes and changes, too many to list here and I have forgot most of them anyway</li><li>Big thanks to MERAS for the comprehensive list of suggestions and improvements to the background map!</li></ul>"
+      },
+    ],
+  },
+
+  {
+    id: "ets2-global-background-map-dark-version",
+    title: "ETS2 Global Background Map - Dark Version",
+    category: "UI",
+    author: "SaltySeeds06",
+    images: ["images/ets2-global-background-map-dark/01.png", "images/ets2-global-background-map-dark/02.png", "images/ets2-global-background-map-dark/03.png", "images/ets2-global-background-map-dark/04.png", "images/ets2-global-background-map-dark/05.png", "images/ets2-global-background-map-dark/06.png"],
+    description: `<p><strong>General Information:</strong></p><p>This background map has full coverage of nearly the entire world</p><p>It adds all of Africa Europe and Asia along with Australia + New Zealand, North America and South America.</p><p>If you have any suggestions or notice any errors, write a comment!</p><p><br></p><p><strong>Notes for Map Modders:</strong></p><p>The
+ majority of the map is based on the Lambert Conic Conformal projection 
+(North America, Asia and Africa above the equator, excluding the 
+mediterranean coast, all follow it exactly) with major adjustments to 
+Europe. Antarctica is entirely fictional</p><p>Australia is based on the
+ same projection however it has been shrunk and moved left due to the 
+Island of Tasmania being beyond the 32 bit integer limit that the game's
+ coordinate system can support.</p><p>South America is based on a custom projection since LCC would make it too large to practically use.</p><p><span style="color: rgb(240, 102, 102);"></p><p><br></p><p><strong>Credits</strong></p><p>Software used: paint.net and QGIS</p><p>During
+ the background map's development it was based on the AllWorld 
+background by Freeman with the borders and outlines of countries have 
+been completely redone from scratch. Full credit to him, and GMC 
+Community for maintaing his background, along with the author of the 
+World's Combined background, Melphiz, and to DOWL for creating the 
+Promods background</p><p>Full credit to BenganJ (rest in peace) who's knowlege of background generation/creation was used extensively for this mod</p>`,
+    etsCompat: "All Game Versions",
+    downloads: [
+      {
+        label: "Download v2.7",
+        url: "https://modsfire.com/8YSLo7O15t1nc81/",
+        changelog: "<p><strong>v2.7</strong></p><ul><li>Made many fixes and changes, too many to list here and I have forgot most of them anyway</li><li>Big thanks to MERAS for the comprehensive list of suggestions and improvements to the background map!</li></ul>"
+      },
+    ],
+  },
+
+  {
+    id: "ets2-global-background-map-dark-grand-utopia",
+    title: "ETS2 Global Background Map - Dark Grand Utopia+",
+    category: "UI",
+    author: "SaltySeeds06",
+    images: ["images/ets2-global-background-map-dark-grand-utopia/01.png", "images/ets2-global-background-map-dark-grand-utopia/02.png", "images/ets2-global-background-map-dark-grand-utopia/03.png", "images/ets2-global-background-map-dark-grand-utopia/04.png"],
+    description: `<p><strong>General Information:</strong></p><p>This background map has 
+full coverage of nearly the entire world including fictional maps such 
+as Grand Utopia, Dreamland Map and fictionally positioned FSG and PJ 
+Indo</p><p>It also adds the same territories as my other backgrounds 
+adding all of Africa Europe and Asia along with Australia + New Zealand,
+ North America and South America.</p><p><br></p><p><strong>Notes for Map Modders:</strong></p><p>The
+ majority of the map is based on the Lambert Conic Conformal projection 
+(North America, Asia and Africa above the equator, excluding the 
+mediterranean coast, all follow it exactly) with major adjustments to 
+Europe. Antarctica is entirely fictional</p><p>Australia is based on the
+ same projection however it has been shrunk and moved left due to the 
+Island of Tasmania being beyond the 32 bit limit that the game's 
+coordinate system can support.</p><p>South America is based on a custom projection since LCC would make it too large to practically use.</p><p><span style="color: rgb(240, 102, 102);"></p><p><br></p><p><strong>Credits</strong></p><p>Software used: paint.net and QGIS</p><p>During
+ the background map's development it was based on the AllWorld 
+background by Freeman with the borders and outlines of countries have 
+been completely redone from scratch. Full credit to him, and GMC 
+Community for maintaing his background, along with the author of the 
+World's Combined background, Melphiz, and to DOWL for creating the 
+Promods background</p><p>Full credit to BenganJ (rest in peace) who's knowlege of background generation/creation was used extensively for this mod</p>`,
+    etsCompat: "All Game Versions",
+    downloads: [
+      {
+        label: "Download v2.7",
+        url: "https://modsfire.com/33UsYH310gEMIOG/",
+        changelog: "<p><strong>v2.7</strong></p><ul><li>Made many fixes and changes, too many to list here and I have forgot most of them anyway</li><li>Big thanks to MERAS for the comprehensive list of suggestions and improvements to the background map!</li></ul>"
+      },
+    ],
+  },
+
+
+  {
+    id: "ets2-global-background-map-scs-style",
+    title: "ETS2 Global Background Map - SCS Style",
+    category: "UI",
+    author: "SaltySeeds06",
+    images: ["images/ets2-global-background-map-scs-style/01.png", "images/ets2-global-background-map-scs-style/02.png", "images/ets2-global-background-map-scs-style/03.png"],
+    description: `<p><strong>General Information:</strong></p><p>This background map has 
+full coverage of nearly the entire world including fictional maps such 
+as Grand Utopia, Dreamland Map and fictionally positioned FSG and PJ 
+Indo</p><p>It also adds the same territories as my other backgrounds 
+adding all of Africa Europe and Asia along with Australia + New Zealand 
+and North America with a small part of northern South America, 
+(including Colombia Venezuela, Guyana, Suriname, French Guiana, Peru, 
+Ecuador, Bolivia, Paraguay, nearly all of Brazil and parts of Argentina 
+and Chile)</p><p>If you have any suggestions or notice any errors, write a comment!</p><p><br></p><p><strong>Notes for Map Modders:</strong></p><p>The
+ majority of the map is based on the Lambert Conic Conformal projection 
+(North America, Asia and Africa above the equator, excluding the 
+mediterranean coast, all follow it exactly) with major adjustments to 
+Europe. Antarctica is entirely fictional</p><p>Australia is based on the
+ same projection however it has been shrunk and moved left due to the 
+Island of Tasmania being beyond the 32 bit limit that the game's 
+coordinate system can support.</p><p>South America is based on a custom projection since LCC would make it too large to practically use.</p><p><br></p><p><strong>Credits</strong></p><p>Software used: paint.net and QGIS</p><p>During
+ the background map's development it was based on the AllWorld 
+background by Freeman with the borders and outlines of countries have 
+been completely redone from scratch. Full credit to him, and GMC 
+Community for maintaing his background, along with the author of the 
+World's Combined background, Melphiz, and to DOWL for creating the 
+Promods background</p><p>Full credit to BenganJ (rest in peace) who's knowlege of background generation/creation was used extensively for this mod</p>`,
+    etsCompat: "All Game Versions",
+    downloads: [
+      {
+        label: "Download v2.7",
+        url: "https://modsfire.com/C89z2oHw53LevOO/",
+        changelog: "<p><strong>v2.7</strong></p><ul><li>Made many fixes and changes, too many to list here and I have forgot most of them anyway</li><li>Big thanks to MERAS for the comprehensive list of suggestions and improvements to the background map!</li></ul>"
+      },
+    ],
+  },
+
+  {
+    id: "eaa-rescaled",
+    title: "EAA Rescaled",
+    category: "Fixes & RCs",
+    author: "SaltySeeds06",
+    images: ["images/eaa_rescaled/01.png", "images/eaa_rescaled/02.png"],
+    description: `<p>A detailed global background map replacement for Euro Truck Simulator 2.</p>
+               <p>Works with all map mods. Place high in your mod manager load order.</p>`,
+    etsCompat: "1.58",
+    downloads: [
+      {
+        label: "Download v2.5.1",
+        url: "https://modsfire.com/M94v4e38TEBC8iV/",
+        changelog: "<p><strong>v2.5.1 for 1.58</strong></p><ul><li>Fixed missing city</li></ul>"
+      },
+    ],
+  },
+
+  {
+    id: "promods-the-great-steppe-beyond-connector",
+    title: "ProMods The Great Steppe - Beyond Connector (ProMods Priority)",
+    category: "Fixes & RCs",
+    author: "saltyleob",
+    images: ["images/beyond_pmtgs/01.png"],
+    description: `<p>A RC between TGS and Beyond that priorities ProMods Areas, namely Garabogaz.</p>
+               <p>Load order in the Mod manager:</p>
+               <p>ProMods The Great Steppe - Beyond Connector</p>
+               <p>Beyond Fix</p>
+               <p>Beyond Files</p>
+               <p>TGS Files</p><br>
+               <p>Note: This RC is made entirely from scratch and is not based on any existing RC's</p>`,
+    etsCompat: "1.58.1",
+    downloads: [
+      {
+        label: "Download v1.58.1",
+        url: "https://modsfire.com/kuAS7ymJ8CZ3tKa/",
+        changelog: "<p><strong>v1.58.1</strong></p><ul><li>Fixed bug (oops)</li></ul>"
+      },
+    ],
+  },
+
+  {
+    id: "project-caribbean",
+    title: "Project Caribbean",
+    category: "Maps",
+    author: "DanielV8/leob",
+    images: ["images/project-caribbean/01.png", "images/project-caribbean/02.png", "images/project-caribbean/03.png", "images/project-caribbean/04.png", "images/project-caribbean/05.png", "images/project-caribbean/06.png", "images/project-caribbean/07.png"],
+    description: `<p>Explore the Caribbean and Central America&nbsp;in ETS2 with this mod today!</p><p>Including the tropical Island of Cuba with the cities of Santiago de Cuba and Nueva Gerona</p><p>And the city of Puerto Barrios in Guatemala ☺️</p><p><br></p><p>Compatibility: Works with all other mods, place anywhere in the load order for it to function</p>`,
+    etsCompat: "1.55 - 1.58",
+    downloads: [
+      {
+        label: "Download v1.58.1",
+        url: "https://modsfire.com/59B550jP6BTCCFS/",
+        changelog: "<p><strong>v1.58.1</strong></p><ul><li>Fixed bug (oops)</li></ul>"
+      },
+    ],
+  },
+
+];
+
