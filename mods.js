6a67d407a726680ba19ee62746b61126328edf82
SaltySeeds06 <saltyseedz@gmail.com>
2026-05-07 19:12:39 +0100

    Hehe

--- mods.js
+++ mods.js
@@ -5,7 +5,7 @@ const MODS = [
     category: "UI",
     author: "SaltySeeds06",
     images: ["images/ets2-global-background-map/00.png", "images/ets2-global-background-map/01.png", "images/ets2-global-background-map/02.png", "images/ets2-global-background-map/03.png", "images/ets2-global-background-map/04.png"],
-    description: `<p><strong>General Information:</strong></p><p>This background map has full coverage of nearly the entire world</p><p>It adds all of Africa Europe and Asia along with Australia + New Zealand, North America and South America.</p><p>If you have any suggestions or notice any errors, write a comment!</p><p><br></p><p><strong>Notes for Map Modders:</strong></p><p>The majority of the map is based on the Lambert Conic Conformal projection (North America, Asia and Africa above the equator, excluding the mediterranean coast, all follow it exactly) with major adjustments to Europe. Antarctica is entirely fictional</p><p>Australia is based on the same projection however it has been shrunk and moved left due to the Island of Tasmania being beyond the limit that the game's coordinate system can support.</p><p>South America is based on a custom projection since LCC would make it too large to practically use.</p><p><span style="color: rgb(240, 102, 102);"></p><p><br></p><p><strong>Credits</strong></p><p>Software used: paint.net and QGIS</p><p>During the background map's development it was based on the AllWorld background by Freeman with the borders and outlines of countries have been completely redone from scratch. Full credit to him, and GMC Community for maintaing his background, along with the author of the World's Combined background, Melphiz, and to DOWL for creating the Promods background</p><p>Full credit to BenganJ (rest in peace) who's knowlege of background generation/creation was used extensively for this mod</p>`,
+    description: `<p><strong>General Information:</strong></p><p>This background map has full coverage of nearly the entire world</p><p>It adds all of Africa Europe and Asia along with Australia + New Zealand, North America and South America.</p><p>If you have any suggestions or notice any errors, write a comment!</p><p><br></p><p><strong>Notes for Map Modders:</strong></p><p>The majority of the map is based on the Lambert Conic Conformal projection (North America, Asia and Africa above the equator, excluding the mediterranean coast, all follow it exactly) with major adjustments to Europe. Antarctica is entirely fictional</p><p>Australia is based on the same projection however it has been shrunk and moved left due to the Island of Tasmania being beyond the limit that the game's coordinate system can support.</p><p>South America is based on a custom projection since LCC would make it too large to practically use.</p><p><br></p><p><strong>Credits</strong></p><p>Software used: paint.net and QGIS</p><p>During the background map's development it was based on the AllWorld background by Freeman with the borders and outlines of countries have been completely redone from scratch. Full credit to him, and GMC Community for maintaing his background, along with the author of the World's Combined background, Melphiz, and to DOWL for creating the Promods background</p><p>Full credit to BenganJ (rest in peace) who's knowlege of background generation/creation was used extensively for this mod</p>`,
     etsCompat: "All Game Versions",
     downloads: [
       {
@@ -37,7 +37,7 @@ mediterranean coast, all follow it exactly) with major adjustments to
 Europe. Antarctica is entirely fictional</p><p>Australia is based on the
  same projection however it has been shrunk and moved left due to the 
 Island of Tasmania being beyond the 32 bit limit that the game's 
-coordinate system can support.</p><p>South America is based on a custom projection since LCC would make it too large to practically use.</p><p><span style="color: rgb(240, 102, 102);"></p><p><br></p><p><strong>Credits</strong></p><p>Software used: paint.net and QGIS</p><p>During
+coordinate system can support.</p><p>South America is based on a custom projection since LCC would make it too large to practically use.</p><p><br></p><p><strong>Credits</strong></p><p>Software used: paint.net and QGIS</p><p>During
  the background map's development it was based on the AllWorld 
 background by Freeman with the borders and outlines of countries have 
 been completely redone from scratch. Full credit to him, and GMC 
@@ -67,7 +67,7 @@ mediterranean coast, all follow it exactly) with major adjustments to
 Europe. Antarctica is entirely fictional</p><p>Australia is based on the
  same projection however it has been shrunk and moved left due to the 
 Island of Tasmania being beyond the 32 bit integer limit that the game's
- coordinate system can support.</p><p>South America is based on a custom projection since LCC would make it too large to practically use.</p><p><span style="color: rgb(240, 102, 102);"></p><p><br></p><p><strong>Credits</strong></p><p>Software used: paint.net and QGIS</p><p>During
+ coordinate system can support.</p><p>South America is based on a custom projection since LCC would make it too large to practically use.</p><p></p><p><br></p><p><strong>Credits</strong></p><p>Software used: paint.net and QGIS</p><p>During
  the background map's development it was based on the AllWorld 
 background by Freeman with the borders and outlines of countries have 
 been completely redone from scratch. Full credit to him, and GMC 
@@ -102,7 +102,7 @@ mediterranean coast, all follow it exactly) with major adjustments to
 Europe. Antarctica is entirely fictional</p><p>Australia is based on the
  same projection however it has been shrunk and moved left due to the 
 Island of Tasmania being beyond the 32 bit limit that the game's 
-coordinate system can support.</p><p>South America is based on a custom projection since LCC would make it too large to practically use.</p><p><span style="color: rgb(240, 102, 102);"></p><p><br></p><p><strong>Credits</strong></p><p>Software used: paint.net and QGIS</p><p>During
+coordinate system can support.</p><p>South America is based on a custom projection since LCC would make it too large to practically use.</p><p><br></p><p><strong>Credits</strong></p><p>Software used: paint.net and QGIS</p><p>During
  the background map's development it was based on the AllWorld 
 background by Freeman with the borders and outlines of countries have 
 been completely redone from scratch. Full credit to him, and GMC 
@@ -209,12 +209,47 @@ and fixes many issues that EAA had when being combined with other maps -
     author: "DanielV8/leob",
     images: ["images/project-caribbean/01.png", "images/project-caribbean/02.png", "images/project-caribbean/03.png", "images/project-caribbean/04.png", "images/project-caribbean/05.png", "images/project-caribbean/06.png", "images/project-caribbean/07.png"],
     description: `<p>Explore the Caribbean and Central America&nbsp;in ETS2 with this mod today!</p><p>Including the tropical Island of Cuba with the cities of Santiago de Cuba and Nueva Gerona</p><p>And the city of Puerto Barrios in Guatemala ☺️</p><p><br></p><p>Compatibility: Works with all other mods, place anywhere in the load order for it to function</p>`,
-    etsCompat: "1.55 - 1.58",
+    etsCompat: "1.55",
     downloads: [
       {
-        label: "Download v1.58.1",
+        label: "Download v1.55",
         url: "https://modsfire.com/59B550jP6BTCCFS/",
-        changelog: "<p><strong>v1.58.1</strong></p><ul><li>Fixed bug (oops)</li></ul>"
+        changelog: "<p><strong>v1.55</strong></p><ul><li>Added Nueva Gerona in Cuba and made improvements to Santiago de Cuba</li><li>New Country! - Guatemala with the city Puerto Barrios</li></ul>"
+      },
+    ],
+  },
+
+
+  {
+    id: "trans-siberian-truckway",
+    title: "Trans-Siberian Truckway",
+    category: "Maps",
+    author: "kwigdulah, ВЛАДЕЛЕЦ ДРУГ",
+    images: ["images/trans-siberian-truckway/01.png", "images/trans-siberian-truckway/02.png", "images/trans-siberian-truckway/03.png", "images/trans-siberian-truckway/04.png", "images/trans-siberian-truckway/05.png", "images/trans-siberian-truckway/06.png", "images/trans-siberian-truckway/07.png", "images/trans-siberian-truckway/08.png", "images/trans-siberian-truckway/09.png"],
+    description: `Map of Russia (Siberia and the Far East) from Omsk Oblast to Vladivostok through Lake Baikal, Amur river and the Sakha republic.&nbsp;<br>More than 6000 km of roads. nearly 60 cities, however most are made with haste and most don’t look like real ones.&nbsp;<br>The map will be rebuilt city by city and road by road, the largest of these being Novosibirsk, along with sections in Amur Oblast and Yakutia<br>There are several connections to different maps including:<br><br>SibirMap - Free and Paid<br>﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿Off the Grid<br>Altai Map (requires a separate RC since Altai 1.7)<br>Beyond Map (Road to Asia)<br>Truck Dream Road (Map of China)<br>Far East Russia<br>Amur Map<br><br><br>The map is designed to be used as a connector until new maps are released. If you are a creator and can make any of these regions better, do so, you are able to build on our sectors, and you will initially have a road with the rest of Russia. No more isolated territories!<br>Requirements - ALL MAP DLCS ARE REQUIRED<br>Load order: place above other maps with connectors at the top.<br><br><br>Credits:<br><br>kwigdulah (Kalachinsk to Khabarovsk)<br>Владелец Друг (Khabarovsk to Tonghua)<br>Tavlov_KZ (def files and testing)<br>hi sora (general assistance)<br>Владимир Моисеев for permission to adapt his Novosibirsk Map to 1:19 Scale!<br>Boberowsky and UniMaps for permssion to integrate Belt of the Tundra<br>manulpallascat for permission to integrate Wakkanai Map`,    
+    etsCompat: "1.59",
+    downloads: [
+      {
+        label: "Download v3.2",
+        url: "https://mods.to/0BUq69fcbcf22237f/",
+        changelog: "<p><strong>3.2</strong></p><ul><li>Fixed bugs</li><li>Updated to 1.59</li></ul>"
+      },
+    ],
+  },
+
+  {
+    id: "trans-siberian-truckway-sibirmap-rc",
+    title: "Trans-Siberian Truckway - SibirMap RC",
+    category: "Fixes & RCs",
+    author: "kwigdulah, ВЛАДЕЛЕЦ ДРУГ",
+    images: ["images/trans-siberian-truckway/01.png"],
+    description: `<p>Load order- place above both maps</p>`,
+    etsCompat: "1.59",
+    downloads: [
+      {
+        label: "Download v3.2",
+        url: "https://mods.to/UTHx69fcbdb05e815/",
+        changelog: "<p><strong>v3.2</strong></p><ul><li>Updated to 1.59</li></ul>"
       },
     ],
   },
