963086efdc173226cc63a8a1df0b10317509ea7b
SaltySeeds06 <saltyseedz@gmail.com>
2026-08-16 02:09:29 +0100

    dfhdfhdfhdfg

--- mods.js
+++ mods.js
@@ -381,6 +381,23 @@ and fixes many issues that EAA had when being combined with other maps -
   },
 
   {
+    id: "trans-siberian-truckway-amur-map-rc",
+    title: "Trans-Siberian Truckway - Amur Map RC",
+    category: "Fixes & RCs",
+    author: "kwigdulah, ВЛАДЕЛЕЦ ДРУГ",
+    images: ["images/trans-siberian-truckway/01.png"],
+    description: `<p>Load order- place above both maps</p>`,
+    etsCompat: "1.60",
+    downloads: [
+      {
+        label: "Download v3.4",
+        url: "https://mods.to/PQrD6a810dae55af8/",
+        changelog: "<p><strong>v3.3</strong></p><ul><li>Updated to 1.60</li></ul>"
+      },
+    ],
+  },
+
+  {
     id: "riw-maghreb-rc",
     title: "RIW PM Maghreb RC",
     category: "Fixes & RCs",
@@ -407,28 +424,6 @@ and fixes many issues that EAA had when being combined with other maps -
       {
         label: "Download v1.59",
         url: "https://modsfire.com/DM8KqQ1B19St718/",
-        changelog: "<p><strong>v1.59</strong></p><ul><li>Initial Release</li></ul>"
-      },
-    ],
-  },
-
-  {
-    id: "cnx-pm-eu-rm",
-    title: "ProMods RusMap Connector",
-    category: "Fixes & RCs",
-    author: "leob",
-    images: ["images/cnx-pm-eu-rm/01.png"],
-    description: `<p>Load order:</p><p>ProMods RusMap Connector</p><p>RusMap Files</p><p>ProMods files</p>`,
-    etsCompat: "1.60",
-    downloads: [
-      {
-        label: "Download v1.60",
-        url: "https://modsfire.com/K68tK16p51gc60D/",
-        changelog: "<p><strong>v1.60</strong></p><ul><li>Updated to 1.60</li></ul>"
-      },
-      {
-        label: "Download v1.59",
-        url: "https://mods.to/1MIw6a09fb40629d7/",
         changelog: "<p><strong>v1.59</strong></p><ul><li>Initial Release</li></ul>"
       },
     ],
